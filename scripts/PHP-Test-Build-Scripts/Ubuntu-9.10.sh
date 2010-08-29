3b3da07ec3b3b0a6d4c547435f1feb0ab5880961
Till Klampaeckel <till@php.net>
2010-08-29 15:19:10 +0200

    * fix this script, refactor calls

--- scripts/PHP-Test-Build-Scripts/Ubuntu-9.10.sh
+++ scripts/PHP-Test-Build-Scripts/Ubuntu-9.10.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 ################################################################################
 # PURPOSE: Builds a PHP testing environment for Ubuntu 9.10.                   #
@@ -13,99 +13,92 @@ help_state="no"
 cached_state="no"
 build_state="all"
 force_state="no"
-gcov="no"
 
+php52_src_dir="php52"
+php53_src_dir="php53"
 phptrunk_src_dir="php-trunk"
 
+php52_snaps="http://snaps.php.net/php5.2-latest.tar.gz"
+php52_archive="php52.tgz"
+php53_snaps="http://snaps.php.net/php5.3-latest.tar.gz"
+php53_archive="php53.tgz"
 phptrunk_snaps="http://snaps.php.net/php-trunk-latest.tar.gz"
 phptrunk_archive="php-trunk.tgz"
 
+install_prefix="/usr/local"
+install_exts="--enable-gcov --enable-bcmath --enable-calendar --enable-ftp --enable-shmop --enable-sockets --enable-exif --with-zlib --with-kerberos --enable-wddx --enable-zip --with-mime-magic --enable-mbstring --enable-mbregex --enable-pcntl --enable-sysvsem --enable-sysvshm --enable-sysvmsg --with-regex --enable-soap"
+
+
 # Loop through the arguments and set state.
 for arg; do
-  case $arg in
-    --help)
-      help_state="yes" ;;
-    --gcov)
-      gcov="yes" ;;
-    --cached)
-      cached_state="yes" ;;
-    --force)
-      force_state="yes" ;;
-  esac
+    case $arg in
+        --help)
+            help_state="yes" ;;
+        --cached)
+            cached_state="yes" ;;
+        --php52)
+            build_state="52" ;;
+        --php53)
+            build_state="53" ;;
+        --phptrunk)
+            build_state="trunk" ;;
+        --force)
+            force_state="yes" ;;
+    esac
 done
+
+source ./functions
 
 # Show help.
 if [ $help_state = "yes" ]; then
-  echo "Usage: $0 [options]"
-  echo "  --help     = Show usage and options."
-  echo "  --cached   = Attempt to use a cached file, keep downloads cached."
-  echo "  --force    = Overwrite existing files if present."
-  echo "  --gcov     = enable gcov"
-  exit 0
+    echo "Usage: $0 [options]"
+    echo "  --help     = Show usage and options."
+    echo "  --cached   = Attempt to use a cached file, keep downloads cached."
+    echo "  --force    = Overwrite existing files if present."
+    echo "  --php52    = Only build the latest PHP 5.2."
+    echo "  --php53    = Only build the latest PHP 5.3."
+    echo "  --phptrunk = Only build the latest PHP Trunk."
+    exit 0
 fi
 
 # Install required packages.
 echo "Installing required packages."
-sudo aptitude install autoconf build-essential libxml2-dev pkg-config
-if [ "$?" -ne 0 ]; then echo "Failed installing required packages."; exit 1; fi
-
-if [ -d $phptrunk_src_dir ]; then
-  if [ $force_state = "yes" ]; then
-    echo "Cleaning the \"${phptrunk_src_dir}\" directory."
-    rm -rf "./${phptrunk_src_dir}/*"
-    if [ "$?" -ne 0 ]; then echo "Failed while cleaning the \"${phptrunk_src_dir}\" directory."; fi
-  else
-    echo "Error: The directory \"${phptrunk_src_dir}\" already exists."
-    echo "Either delete the directory or use --force to overwrite it."
-    exit 1
-  fi
-else
-  echo "Creating ${phptrunk_src_dir}."
-  mkdir $phptrunk_src_dir
-  if [ "$?" -ne 0 ]; then echo "Failed while creating the ${phptrunk_src_dir} directory."; fi
+sudo aptitude install autoconf build-essential libxml2-dev pkg-config lcov
+if [ "$?" -ne 0 ]; then
+    echo "Failed installing required packages.";
+    exit 1;
 fi
 
-if [ -f $phptrunk_archive ]; then
-  if [ $cached_state = "no" ]; then
-    rm -f $phptrunk_archive
-    if [ "$?" -ne 0 ]; then echo "Failed removing old archive ${phptrunk_archive}."; fi
-  fi
+
+if [ "$build_state" = "all" -o "$build_state" = "52" ]; then
+
+    echo "BUILDING PHP 5.2"
+
+    setup_folders $php52_src_dir
+    download_snaps $php52_archive $php52_snaps
+    extract_snap $php52_archive $php52_src_dir
+    install_snap $php52_src_dir
+    cleanup $php52_archive
 fi
 
-if [ -f $phptrunk_archive -o ! $build_state = "all" -a ! $build_state = "trunk" ]; then
-  echo "Skipping download of ${phptrunk_archive}."
-else 
-  echo "Downloading ${phptrunk_archive} from snaps.php.net."
-  wget -O $phptrunk_archive $phptrunk_snaps
-  if [ "$?" -ne 0 ]; then echo "Failed downloading ${phptrunk_archive} from php.snaps.net."; exit 1; fi
+if [ "$build_state" = "all" -o "$build_state" = "53" ]; then
+
+    echo "BUILDING PHP 5.3"
+
+    setup_folders $php53_src_dir
+    download_snaps $php53_archive $php53_snaps
+    extract_snap $php53_archive $php53_src_dir
+    install_snap $php53_src_dir
+    cleanup $php53_archive
 fi
 
-echo "Extracting ${phptrunk_archive} source."
-tar -xzf $phptrunk_archive --strip=1 -C "./${phptrunk_src_dir}"
-if [ "$?" -ne 0 ]; then echo "Failed extracting ${phptrunk_archive}."; fi
+if [ "$build_state" = "all" -o "$build_state" = "trunk" ]; then
 
-cd ${phptrunk_src_dir}
-if [ "$?" -ne 0 ]; then echo "Failed to change directory to ${phptrunk_src_dir}."; fi
+    echo "BUILDING PHP HEAD"
 
-echo "Configuring the PHP Trunk source."
-config_line="--prefix=/usr/local/$phptrunk_src_dir --enable-bcmath --enable-calendar --enable-ftp --enable-shmop --enable-sockets --enable-exif --with-zlib --with-kerberos --enable-wddx --enable-zip --with-mime-magic --enable-mbstring --enable-mbregex --enable-pcntl --enable-sysvsem --enable-sysvshm --enable-sysvmsg --with-regex --enable-soap"
-if [ $gcov = "yes" ]; then
-  $config_line="$config_line --enable-gcov"
-fi
-
-./configure $config_line
-if [ "$?" -ne 0 ]; then echo "Failed to configure the PHP Trunk source."; fi
-
-echo "Building the PHP Trunk binary."
-make
-if [ "$?" -ne 0 ]; then echo "Failed to build the PHP Trunk binary."; fi
-
-cd ../
-
-
-# Remove archives.
-if [ $cached_state = "no" ]; then
-  echo "Cleaning up ${phptrunk_archive}."
-  rm -f $phptrunk_archive
-  if [ "$?" -ne 0 ]; then echo "Failed to remove ${phptrunk_archive}."; fi
+    setup_folders $phptrunk_src_dir
+    download_snaps $phptrunk_archive $phptrunk_snaps
+    extract_snap $phptrunk_archive $phptrunk_src_dir
+    install_snap $phptrunk_src_dir
+    cleanup $phptrunk_archive
 fi
