2e73337348804bb7abdb40af1ed47f08b1d5fe29
flyingmana <flyingmana@googlemail.com>
2010-08-29 14:05:45 +0200

    modifie ubuntu build script make --php52 --php53 and --phptrunk usable dont let script exit on error anymore (except if the download fails)

--- scripts/PHP-Test-Build-Scripts/Ubuntu-9.10.sh
+++ scripts/PHP-Test-Build-Scripts/Ubuntu-9.10.sh
@@ -67,7 +67,7 @@ if [ -d $php52_src_dir ]; then
   if [ $force_state = "yes" ]; then
     echo "Cleaning the \"${php52_src_dir}\" directory."
     rm -rf "./${php52_src_dir}/*"
-    if [ "$?" -ne 0 ]; then echo "Failed while cleaning the \"${php52_src_dir}\" directory."; exit 1; fi
+    if [ "$?" -ne 0 ]; then echo "Failed while cleaning the \"${php52_src_dir}\" directory."; fi
   else
     echo "Error: The directory \"${php52_src_dir}\" already exists."
     echo "Either delete the directory or use --force to overwrite it."
@@ -76,14 +76,14 @@ if [ -d $php52_src_dir ]; then
 else
   echo "Creating ${php52_src_dir}."
   mkdir $php52_src_dir
-  if [ "$?" -ne 0 ]; then echo "Failed while creating the ${php52_src_dir} directory."; exit 1; fi
+  if [ "$?" -ne 0 ]; then echo "Failed while creating the ${php52_src_dir} directory."; fi
 fi
 
 if [ -d $php53_src_dir ]; then
   if [ $force_state = "yes" ]; then
     echo "Cleaning the \"${php53_src_dir}\" directory."
     rm -rf "./${php53_src_dir}/*"
-    if [ "$?" -ne 0 ]; then echo "Failed while cleaning the \"${php53_src_dir}\" directory."; exit 1; fi
+    if [ "$?" -ne 0 ]; then echo "Failed while cleaning the \"${php53_src_dir}\" directory."; fi
   else
     echo "Error: The directory \"${php53_src_dir}\" already exists."
     echo "Either delete the directory or use --force to overwrite it."
@@ -92,14 +92,14 @@ if [ -d $php53_src_dir ]; then
 else
   echo "Creating ${php53_src_dir}."
   mkdir $php53_src_dir
-  if [ "$?" -ne 0 ]; then echo "Failed while creating the ${php53_src_dir} directory."; exit 1; fi
+  if [ "$?" -ne 0 ]; then echo "Failed while creating the ${php53_src_dir} directory."; fi
 fi
 
 if [ -d $phptrunk_src_dir ]; then
   if [ $force_state = "yes" ]; then
     echo "Cleaning the \"${phptrunk_src_dir}\" directory."
     rm -rf "./${phptrunk_src_dir}/*"
-    if [ "$?" -ne 0 ]; then echo "Failed while cleaning the \"${phptrunk_src_dir}\" directory."; exit 1; fi
+    if [ "$?" -ne 0 ]; then echo "Failed while cleaning the \"${phptrunk_src_dir}\" directory."; fi
   else
     echo "Error: The directory \"${phptrunk_src_dir}\" already exists."
     echo "Either delete the directory or use --force to overwrite it."
@@ -108,7 +108,7 @@ if [ -d $phptrunk_src_dir ]; then
 else
   echo "Creating ${phptrunk_src_dir}."
   mkdir $phptrunk_src_dir
-  if [ "$?" -ne 0 ]; then echo "Failed while creating the ${phptrunk_src_dir} directory."; exit 1; fi
+  if [ "$?" -ne 0 ]; then echo "Failed while creating the ${phptrunk_src_dir} directory."; fi
 fi
 
 
@@ -116,11 +116,11 @@ fi
 if [ -f $php52_archive ]; then
   if [ $cached_state = "no" ]; then
     rm -f $php52_archive
-    if [ "$?" -ne 0 ]; then echo "Failed removing old archive ${php52_archive}."; exit 1; fi
+    if [ "$?" -ne 0 ]; then echo "Failed removing old archive ${php52_archive}."; fi
   fi
 fi
 
-if [ -f $php52_archive ]; then
+if [ -f $php52_archive -o ! $build_state = "all" -a ! $build_state = "52" ]; then
   echo "Skipping download of ${php52_archive}."
 else 
   echo "Downloading ${php52_archive} from snaps.php.net."
@@ -131,11 +131,11 @@ fi
 if [ -f $php53_archive ]; then
   if [ $cached_state = "no" ]; then
     rm -f $php53_archive
-    if [ "$?" -ne 0 ]; then echo "Failed removing old archive ${php53_archive}."; exit 1; fi
+    if [ "$?" -ne 0 ]; then echo "Failed removing old archive ${php53_archive}."; fi
   fi
 fi
 
-if [ -f $php53_archive ]; then
+if [ -f $php53_archive -o ! $build_state = "all" -a ! $build_state = "53" ]; then
   echo "Skipping download of ${php53_archive}."
 else 
   echo "Downloading ${php53_archive} from snaps.php.net."
@@ -146,11 +146,11 @@ fi
 if [ -f $phptrunk_archive ]; then
   if [ $cached_state = "no" ]; then
     rm -f $phptrunk_archive
-    if [ "$?" -ne 0 ]; then echo "Failed removing old archive ${phptrunk_archive}."; exit 1; fi
+    if [ "$?" -ne 0 ]; then echo "Failed removing old archive ${phptrunk_archive}."; fi
   fi
 fi
 
-if [ -f $phptrunk_archive ]; then
+if [ -f $phptrunk_archive -o ! $build_state = "all" -a ! $build_state = "trunk" ]; then
   echo "Skipping download of ${phptrunk_archive}."
 else 
   echo "Downloading ${phptrunk_archive} from snaps.php.net."
@@ -162,50 +162,50 @@ fi
 # Extract archives into source directories.
 echo "Extracting ${php52_archive} source."
 tar -xzf $php52_archive --strip=1 -C "./${php52_src_dir}"
-if [ "$?" -ne 0 ]; then echo "Failed extracting ${php52_archive}."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed extracting ${php52_archive}."; fi
 
 echo "Extracting ${php53_archive} source."
 tar -xzf $php53_archive --strip=1 -C "./${php53_src_dir}"
-if [ "$?" -ne 0 ]; then echo "Failed extracting ${php53_archive}."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed extracting ${php53_archive}."; fi
 
 echo "Extracting ${phptrunk_archive} source."
 tar -xzf $phptrunk_archive --strip=1 -C "./${phptrunk_src_dir}"
-if [ "$?" -ne 0 ]; then echo "Failed extracting ${phptrunk_archive}."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed extracting ${phptrunk_archive}."; fi
 
 
 # Configure and build PHP.
 cd $php52_src_dir
-if [ "$?" -ne 0 ]; then echo "Failed to change directory to ${php52_src_dir}."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to change directory to ${php52_src_dir}."; fi
 
 echo "Configuring the PHP 5.2 source."
 ./configure --prefix=/usr/local/$php52_src_dir --enable-bcmath --enable-calendar --enable-ftp --enable-shmop --enable-sockets --enable-exif --with-zlib --with-kerberos --enable-wddx --enable-zip --with-mime-magic --enable-mbstring --enable-mbregex --enable-pcntl --enable-sysvsem --enable-sysvshm --enable-sysvmsg --with-regex --enable-soap
-if [ "$?" -ne 0 ]; then echo "Failed to configure the PHP 5.2 source."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to configure the PHP 5.2 source."; fi
 
 echo "Building the PHP 5.2 binary."
 make
-if [ "$?" -ne 0 ]; then echo "Failed to build the PHP 5.2 binary."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to build the PHP 5.2 binary."; fi
 
 cd "../${php53_src_dir}"
-if [ "$?" -ne 0 ]; then echo "Failed to change directory to ${php53_src_dir}."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to change directory to ${php53_src_dir}."; fi
 
 echo "Configuring the PHP 5.3 source."
 ./configure --prefix=/usr/local/$php53_src_dir --enable-bcmath --enable-calendar --enable-ftp --enable-shmop --enable-sockets --enable-exif --with-zlib --with-kerberos --enable-wddx --enable-zip --with-mime-magic --enable-mbstring --enable-mbregex --enable-pcntl --enable-sysvsem --enable-sysvshm --enable-sysvmsg --with-regex --enable-soap
-if [ "$?" -ne 0 ]; then echo "Failed to configure the PHP 5.3 source."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to configure the PHP 5.3 source."; fi
 
 echo "Building the PHP 5.3 binary."
 make
-if [ "$?" -ne 0 ]; then echo "Failed to build the PHP 5.3 binary."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to build the PHP 5.3 binary."; fi
 
 cd "../${phptrunk_src_dir}"
-if [ "$?" -ne 0 ]; then echo "Failed to change directory to ${phptrunk_src_dir}."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to change directory to ${phptrunk_src_dir}."; fi
 
 echo "Configuring the PHP Trunk source."
 ./configure --prefix=/usr/local/$phptrunk_src_dir --enable-bcmath --enable-calendar --enable-ftp --enable-shmop --enable-sockets --enable-exif --with-zlib --with-kerberos --enable-wddx --enable-zip --with-mime-magic --enable-mbstring --enable-mbregex --enable-pcntl --enable-sysvsem --enable-sysvshm --enable-sysvmsg --with-regex --enable-soap
-if [ "$?" -ne 0 ]; then echo "Failed to configure the PHP Trunk source."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to configure the PHP Trunk source."; fi
 
 echo "Building the PHP Trunk binary."
 make
-if [ "$?" -ne 0 ]; then echo "Failed to build the PHP Trunk binary."; exit 1; fi
+if [ "$?" -ne 0 ]; then echo "Failed to build the PHP Trunk binary."; fi
 
 cd ../
 
@@ -214,13 +214,13 @@ cd ../
 if [ $cached_state = "no" ]; then
   echo "Cleaning up ${php52_archive}."
   rm -f $php52_archive
-  if [ "$?" -ne 0 ]; then echo "Failed to remove ${php52_archive}."; exit 1; fi
+  if [ "$?" -ne 0 ]; then echo "Failed to remove ${php52_archive}."; fi
   
   echo "Cleaning up ${php53_archive}."
   rm -f $php53_archive
-  if [ "$?" -ne 0 ]; then echo "Failed to remove ${php53_archive}."; exit 1; fi
+  if [ "$?" -ne 0 ]; then echo "Failed to remove ${php53_archive}."; fi
   
   echo "Cleaning up ${phptrunk_archive}."
   rm -f $phptrunk_archive
-  if [ "$?" -ne 0 ]; then echo "Failed to remove ${phptrunk_archive}."; exit 1; fi
+  if [ "$?" -ne 0 ]; then echo "Failed to remove ${phptrunk_archive}."; fi
 fi
